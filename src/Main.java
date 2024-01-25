06f5c9921a44aeb82160bc9b93490886ee737f98
Kseniia Bondarenko <bondarenkoksenia2007@gmail.com>
2024-01-25 15:47:55 +0200

    2nd

--- src/Main.java
+++ src/Main.java
@@ -2,77 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) {
-        task1(1);
-        task1(0);
-        task1(-3);
 
-        task2(1);
-        task2(0);
-        task2(-3);
+        public static void main(String[] args) {
+            int val = 5;
+            int[] arr = {-1,-3,-5,-4};
+            int[] arr2 = {5,-3,-2,16,55,0};
+            task1();
 
-        task3(1);
-        task3(0);
-        task3(-3);
+            task2();
 
-        task4(1);
-        task4(0);
-        task4(-3);
+            task3();
 
-        task5(1);
-        task5(0);
-        task5(-3);
+            task4();
 
-        task6(1);
-        task6(0);
-        task6(-3);
+            task5();
 
-        task7("test");
-        task7("тест");
-        task7("3");
+            task6();
 
-        task8("1");
-        task8("1");
-        task8("3");
+            max(arr);
 
-        task9(true);
-        task9(false);
+            task7();
 
-        task10(true);
-        task10(false);
-
-        task11(5);
-        task11(0);
-        task11(-3);
-        task11(2);
-
-        task12(5);
-        task12(0);
-        task12(-3);
-        task12(2);
-
-        task13(1, 3);
-        task13(0, 6);
-        task13(3, 5);
-
-        task14(1, 3);
-        task14(0, 6);
-        task14(3, 5);
-
-        task15(1);
-        task15(0);
-        task15(3);
-        task15(2);
-
-        task16(3);
-        task16(-5);
-        task16(13);
-        task16(21);
-
-        task17(3);
-        task17(-5);
-        task17(12);
-        task17(9);
+            fib();
 
 
 
@@ -81,184 +32,105 @@ public class Main {
 
 
 
-
-
-
-
-
-    }
-
-    public static void task1(int a){
-        if (a==0)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
+        }
+        public static void max(int[] arr){
+            int max = arr[0];
+            for (int i = 0; i < arr.length; i++) {
+                if (max< arr[i]){
+                    max = arr[i];
+                }
+            }
+            System.out.println("max = "+max);
         }
 
-    public static void task2(int a){
-        if (a>0)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task3(int a){
-        if (a<0)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task4(int a){
-        if (a>=0)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task5(int a){
-        if (a<=0)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task6(int a){
-        if (a!=0)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-    public static void task7(String a){
-        if (a=="test")
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task8(String a){
-       if (a=="1")
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task9(boolean test){
-        if (test==true)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-        //if (test == true? "right":"wrong");
-    }
-
-    public static void task10(boolean test){
-        if (test!=true)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-
-    }
-
-    public static void task11(int a) {
-        if (a > 0 && a < 5)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-    }
-
-    public static void task12(double a) {
-        if (a == 0 || a == 2) {
-            a = a + 7;
-            System.out.println(a);
+    public static void min(int[] arr){
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min> arr[i]){
+                min = arr[i];
+            }
         }
-        else{
-            a = a / 10;
-            System.out.println(a);
+        System.out.println("min = "+min);
+    }
+
+
+
+    public static void task1(){
+        for (int i = 0 ; i<=110 ; i++) {
+            if (i % 2 == 1)
+                System.out.println(i);
         }
 
     }
 
-    public static void task13(int a, int b) {
-        if (a <=1 && b >= 3) {
-            int sum = a + b;
-            System.out.println(sum);
-        }
-        else{
-            double minus = a - b;
-            System.out.println(minus);
-        }
-
-    }
-    //sout
-
-    public static void task14(int a, int b) {
-        if ( a>=2 && a <=11 || b >= 6 && b<=14)
-            System.out.println("Right");
-        else
-            System.out.println("Wrong");
-
+    public static void task2(){
+        for (int i = 90 ; i>=0 ; i-=5)
+            System.out.println(i);
 
     }
 
-    public static void task15(int num) {
-        switch (num){
-            case 1:
-                String result1 = "Winter";
-                System.out.println(result1);
-                break;
-            case 2:
-                String result2 = "Spring";
-                System.out.println(result2);
-                break;
-            case 3:
-                String result3 = "Summer";
-                System.out.println(result3);
-                break;
-            case 4:
-                String result4 = "Fall";
-                System.out.println(result4);
-                break;
-            default:
-                System.out.println("Oooops, something wrong !");
+    public static void task3(){
+        for (int i = 1 ; i<=40 ; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
 
+    }
+
+    public static void task4(){
+            int el = 2;
+        for (int i = 0 ; i<=20 ; i++) {
+            el*=2;
+            System.out.println(el);
         }
 
 
     }
 
-    public static void task16(int day) {
-        if (day > 0 && day <=10)
-            System.out.println("1st decade");
-        else if (day > 10 && day <=20)
-            System.out.println("2nd decade");
-        else if (day > 20 && day <=31)
-            System.out.println("3rd decade");
-        else
-            System.out.println("Smth wrong...");
+    public static void task5(){
+        for (int i = -29 ; i<=10 ; i++) {
+            if (i % 5 == 0)
+                System.out.println(i);
+        }
+
     }
 
-    public static void task17(int month) {
-        if (month > 0 && month <=2 || month == 12)
-            System.out.println("Winter");
-        else if (month > 2 && month <=5 )
-            System.out.println("Spring");
-        else if (month > 5 && month <=8)
-            System.out.println("Summer");
-        else if (month > 8 && month <=11)
-            System.out.println("Fall");
-        else
-            System.out.println("Smth wrong...");
+    public static void task6(){
+            int sum1 = 0;
+            int even = 0;
+        for (int i = 50 ; i<=150 ; i++) {
+            sum1 += i;
+            if (i % 2 == 0)
+                even++;
+        }
+        System.out.println("Sum: "+sum1+"\t"+"Even: "+even);
+
     }
 
-    //public static void task18(String[] args) {
-        //String inputString = "abcde";
-        //System.out.println(inputString.startsWith("a") ? "yes" : "no");
-    //} Умова передбачає те, що ми ще не знаємо
+    public static void task7(){
+        int[] arr2 = {5,-3,-2,16,55,0};
+        int sum1 = 0;
+        int pos = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            sum1 += arr2[i];
+            if (arr2[i] % 2 == 0)
+                pos++;
+        }
+        System.out.println("Sum: "+sum1+"\t"+"Even: "+pos);
 
+    }
 
+    static void fib() {
+        int first = 1;
+        int second = 1;
 
-
-
+        for (int i = 0; i <= 11; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
 
 
 
