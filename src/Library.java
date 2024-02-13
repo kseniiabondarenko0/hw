ee246d760ea1e9d1924ab2e689cbcdb59c3a08ac
Kseniia Bondarenko <bondarenkoksenia2007@gmail.com>
2024-02-13 18:50:27 +0200

    task with books Library, Book, MainBookTask
    task with manager
    Main, Student, Grade, StudentManager

--- src/Library.java
+++ src/Library.java
@@ -1,54 +1,34 @@
 public class Library {
 
-    Book[] books;
-
-    Library(){
-        books = new Book[10];
-    }
-
+    Book[] books = new Book[10];
     int len;
 
     public void addBook(Book book) {
-        books[len] = book;
-        len++;
+        if (len < books.length) {
+            books[len] = book;
+            len++;
+        }
     }
-
-
-    public void removeBook(String bookTitle) {
-        int indexToRemove = -1;
-        // Знаходимо індекс книги, яку потрібно видалити
+    public void removeBook(String bookName) {
         for (int i = 0; i < len; i++) {
-            if (books[i].title.equals(bookTitle)) {
-                indexToRemove = i;
-                break;
+            if ( bookName == books[i].title) {
+
+                for (int j = i; j < len+1; j++) {
+                    books[j]=books[j+1];
+                }
+
+                len--;
             }
         }
 
-        // Якщо книга знайдена, видаляємо її
-        if (indexToRemove != -1) {
-            // Створюємо новий масив на 1 елемент менший за поточний
-            Book[] newBooks = new Book[len - 1];
-            // Копіюємо елементи до індекса, який потрібно видалити
-            if (indexToRemove > 0) {
-                System.arraycopy(books, 0, newBooks, 0, indexToRemove);
-            }
-            // Копіюємо елементи після видалення
-            if (indexToRemove < len - 1) {
-                System.arraycopy(books, indexToRemove + 1, newBooks, indexToRemove, len - indexToRemove - 1);
-            }
-            // Призначаємо новий масив як список книг
-            books = newBooks;
-            // Зменшуємо лічильник кількості книг
-            len--;
-        } else {
-            System.out.println("Book not found: " + bookTitle);
-        }
     }
-
-    public void print(){
-        for (int i = 0; i< books.length; i++){
-            System.out.print(i+1+" ");
+    public void print() {
+        for (int i = 0; i < len; i++) {
+            System.out.print(i + 1 + " ");
             books[i].print();
+            System.out.println();
         }
+        System.out.println("-----------------");
     }
 }
+
