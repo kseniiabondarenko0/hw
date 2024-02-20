5bb5d2c66d727600e331e537d4f4429bdc821a87
Kseniia Bondarenko <bondarenkoksenia2007@gmail.com>
2024-02-20 16:21:09 +0200

    hw

--- src/Main.java
+++ src/Main.java
@@ -3,72 +3,55 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(reverse("Hello"));
-        System.out.println(task2("Hello"));
-        System.out.println(Arrays.toString(task5("Hello world")));
-        System.out.println(task4("Hello"));
-        System.out.println(task3("mkam"));
 
+        //task 1
+        Library library = new Library();
+        library.addBook(new Book("Percy Jackson and the sea of monsters", "Rick Riordan", 2007));
+        library.addBook(new Book("Good Omens", "Neil Gaiman", 1990));
+        library.addBook(new Book("Harry Potter", "Joane Rowling", 1997));
+        library.addBook(new Book("Newerwhere", "Neil Gaiman", 2008));
+
+        System.out.println("Books in the library:");
+        library.print();
+
+        System.out.println("Amount of Neil Gaiman's books: " + library.countBooksByAuthor("Neil Gaiman"));
+        System.out.println("Amount of books by the title 'Good Omens': " + library.countBooksByTitle("Good Omens"));
+
+
+
+
+
+
+        //task 2
+        Bank bank = new Bank(10);
+
+        bank.createAccount("12345", 1000);
+        bank.createAccount("67890", 500);
+
+        bank.withdrawFromAccount("67890", 200);
+
+        bank.transferBetweenAccounts("12345", "67890", 300);
+
+
+        bank.blockAccount("12345");
+        bank.unblockAccount("67890");
+
+        bank.printAllAccounts();
+
+
+        //task3
+        Company company = new Company();
+
+        company.addEmployee(new Employee("John", 3000, "Manager", "Sales"));
+        company.addEmployee(new Employee("Alice", 2500, "Engineer", "Engineering"));
+        company.addEmployee(new Employee("Bob", 2800, "Analyst", "Finance"));
+
+        company.printAllEmployees();
+
+        System.out.println("Total salary: " + company.calculateTotalSalary());
+        System.out.println("Average salary: " + company.calculateAverageSalary());
 
     }
 
-    public static String reverse(String str) {
-        String reversetext="";
-        for (int i = str.length() - 1; i>-1; i--) {
-            reversetext+=str.charAt(i);
-        }
-        return reversetext;
-    }
-
-    public static String task2(String str){
-        int count = 0;
-        String upperCaseStr = str.toUpperCase();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = upperCaseStr.charAt(i);
-            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-                count++;
-        }
-        String result = "Кількість голосних літер у рядку: " + count;
-        return result;
-    }
-
-    public static boolean task3(String str) {
-        if (str == null)
-            return false;
-        int length = str.length();
-        System.out.println(length / 2);
-        for (int i = 0; i < length / 2; i++) {
-
-            if (str.charAt(i) != str.charAt(length - i - 1))
-                return false;
-        }
-        return true;
-    }
-
-    public static String task4(String str){
-        char[] chars = str.toCharArray();
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < chars.length; i++) {
-            boolean isDuplicate = false;
-            for (int j = 0; j < i; j++) {
-                if (chars[i] == chars[j]) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if (!isDuplicate) {
-                result.append(chars[i]);
-            }
-        }
-
-        return result.toString();
-    }
-
-
-    public static String[] task5(String str) {
-
-        return str.split(" ");
-    }
 
 }
