8cd7bea6d9cb0ad61d930de83a2d2028dabbaedf
Kseniia Bondarenko <bondarenkoksenia2007@gmail.com>
2024-02-24 11:15:43 +0200

    hw 12

--- src/Main.java
+++ src/Main.java
@@ -4,54 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //task 1
-        Library library = new Library();
-        library.addBook(new Book("Percy Jackson and the sea of monsters", "Rick Riordan", 2007));
-        library.addBook(new Book("Good Omens", "Neil Gaiman", 1990));
-        library.addBook(new Book("Harry Potter", "Joane Rowling", 1997));
-        library.addBook(new Book("Newerwhere", "Neil Gaiman", 2008));
+        //task1
+        Product product1 = new Product("Лижі", 2000, 1);
+        Product product2 = new Product("Шолом", 500, 1);
 
-        System.out.println("Books in the library:");
-        library.print();
+        Order order = new Order("Іван Петров", 235, 10);
+        order.addProduct(product1);
+        order.addProduct(product2);
+        order.removeProduct(product1);
 
-        System.out.println("Amount of Neil Gaiman's books: " + library.countBooksByAuthor("Neil Gaiman"));
-        System.out.println("Amount of books by the title 'Good Omens': " + library.countBooksByTitle("Good Omens"));
+        System.out.println("Загальна вартість замовлення: " + order.getTotalPrice());
 
 
+        //task2
+        Shape rectangle = new Rectangle(10, 20, "blue", 30, 40);
+        rectangle.draw();
+        rectangle.resize(50, 60);
+        rectangle.move(100, 150);
+        rectangle.printCoordinates();
+        rectangle.resize(4, 5);
+        rectangle.area();
 
-
-
-
-        //task 2
-        Bank bank = new Bank(10);
-
-        bank.createAccount("12345", 1000);
-        bank.createAccount("67890", 500);
-
-        bank.withdrawFromAccount("67890", 200);
-
-        bank.transferBetweenAccounts("12345", "67890", 300);
-
-
-        bank.blockAccount("12345");
-        bank.unblockAccount("67890");
-
-        bank.printAllAccounts();
-
-
-        //task3
-        Company company = new Company();
-
-        company.addEmployee(new Employee("John", 3000, "Manager", "Sales"));
-        company.addEmployee(new Employee("Alice", 2500, "Engineer", "Engineering"));
-        company.addEmployee(new Employee("Bob", 2800, "Analyst", "Finance"));
-
-        company.printAllEmployees();
-
-        System.out.println("Total salary: " + company.calculateTotalSalary());
-        System.out.println("Average salary: " + company.calculateAverageSalary());
-
+        Shape circle = new Circle(50, 50, "red", 25);
+        circle.draw();
+        circle.resize(40, 40);
+        circle.move(200, 200);
+        circle.printCoordinates();
+        circle.area();
     }
+
 
 
 }
